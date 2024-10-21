283e41d496a2c415df5299be6e3eabd261a75fe7
theAman30 <a.srivastav1005@gmail.com>
2024-10-21 22:24:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+  
+  function scrollToBottom() {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+  }
+  
